8aa749644abf5df314415095ca49e8bdbc45321b
Jindrich Skupa <jindrich.skupa@gmail.com>
2017-03-04 15:45:47 +0100

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 
 apt-get install python python-pip 
-pip install ansible ansible-lint ansible-review
+pip install ansible ansible-lint ansible-review markupsafe
 
 mkdir -p /etc/ansible/{files,roles,playbooks,vars,inventory}
 
